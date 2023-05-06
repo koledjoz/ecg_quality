ed72fbcc90b4d1a5db4550135fa890315fbadf34
@koledjoz <koledjoz@fit.cvut.cz>
2023-05-06 22:44:02 +0200

    Small fixes with setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
 setup(
-  name = 'ecq_quality',
-  packages = ['ecq_quality'],
+  name = 'ecg_quality',
+  packages = ['ecg_quality'],
   version = '0.1.0',
   license='gpl-3.0',
-  description = 'Library that clasifies quality of ECG signal using deep learning methods',
+  description = 'Library that classifies quality of ECG signal using deep learning methods',
   author = 'Jozef Koleda',
   author_email = 'koledjoz@cvut.cz',
-  url = 'https://github.com/koledjoz/ecq_quality',
-  download_url = 'https://github.com/koledjoz/ecq_quality/releases/tag/v0.1.0',
+  url = 'https://github.com/koledjoz/ecg_quality',
+  download_url = 'https://github.com/koledjoz/ecg_quality/releases/tag/v0.1.0',
   keywords = ['ECG', 'quality', 'classification', 'deep learning'],
   install_requires=[
           'tensorflow',
