49dbd9b1ca518eec677338c51a5c729bfc7cd709
@koledjoz <koledjoz@fit.cvut.cz>
2023-05-07 01:11:19 +0200

    Finished setup.py fixes

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setuptools.setup(
   packages = setuptools.find_packages(where='ecg_quality'),
   package_dir = {"": "ecg_quality"},
   include_package_data=True,
-  version = '0.1.0',
+  version = '0.1.1',
   license='gpl-3.0',
   description = 'Library that classifies quality of ECG signal using deep learning methods',
   author = 'Jozef Koleda',
